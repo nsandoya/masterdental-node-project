832f60e7fc77add1c82d4256248d37ec974515cf
nsandoya <natsandoya@gmail.com>
2024-04-29 17:17:53 -0500

    Conexión a ddbb | Servidor implementado

---
+++ ddbb/ddbb.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const mongoURL = 'mongodb+srv://natsandoya:i3GXhg8peY73TE5O@tipti-project.ry83rtv.mongodb.net/master_dental'
+
+// Conexión a la ddbb
+function connectDB(){
+    return new Promise((res, rej) => {
+        mongoose.connect(mongoURL)
+        .then(()=>{
+            console.log("Conectado a tipti-project(master_dental)");
+            res()
+        })
+        .catch((error) => {
+            console.error("Error al conectar a tipti-project(master_dental)");
+            rej(error)
+        })
+    })
+}
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const userRoutes = require("./routes/userRoutes");
+const connectDB  = require('./ddbb/ddbb');
+
+// Crear instancia de Express
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json())
+
+// Rutas:   base     + endpoints
+//app.use('/api/users', userRoutes)
+
+// Inicializar la ddbb
+connectDB()
+
+// Inicializar el servidor
+var server = app.listen(PORT, ()=>{
+    console.log(`App listening in port ${PORT}`)
+})
+server.timeout = 120000; // Tiempo de espera recomendado
