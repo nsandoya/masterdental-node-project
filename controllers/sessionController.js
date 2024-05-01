3b11d34c4a86dc881679f34a4f239a531cb6a038
nsandoya <natsandoya@gmail.com>
2024-05-01 00:47:32 -0500

    getCurrentUser: listo | Pendiente implementación

---
+++ controllers/sessionController.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+// Quién es el usuario activo?
+function getCurrentUser(req, res){
+    new Promise((resolve, reject)=>{
+        const userId = req.userId;
+        
+        // Buscar el usuario usando el id proporcionado
+        User.findById(userId)
+        .then( user => {
+            if(!user){
+                reject({
+                    status: 404,
+                    message: "El usuario no existe"
+                })
+            }else{resolve(user)}
+        })
+        .catch(error => reject({
+            status: 500,
+            message: "Error al tratar de comprobar user identity"
+        }
+        ))
+    })
+    .then(user => res.json(user))
+    .catch(error => {
+        console.log(error)
+        res.status(error.status || 500).send({message: error.message})
+    })
+}
+
+module.exports = {getCurrentUser}
